dae121d097c0cbc667d08809720729320584f82a
aghyad97 <aaa1997aaa@gmail.com>
2020-10-26 23:14:16 +0400

    maps.js

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,29 +1,165 @@
-var locations = [
-  '1600 pennsylvania ave, washington dc',
-  '935 pennsylvania ave, washington dc'
-];
-var zoom = 15;
-var center =  [ 38.895345, -77.030101 ];
+const formId = "form"; // ID of the form
+const url = location.href; //  href for the page
+const formIdentifier = `${url} ${formId}`; // Identifier used to identify the form
+const submitButton = document.querySelector("#submit"); // select save button
+let form = document.querySelector(`#${formId}`); // select form
+let formElements = form.elements; // get the elements in the form
+const alertBox = document.querySelector(".alert"); // select alert display div
 
-function createRoute(_locations, _zoom, _center) {
+const getFormData = () => {
+  let data = {
+    [formIdentifier]: {}
+  }; // create an empty object with the formIdentifier as the key and an empty object as its value
+  for (const element of formElements) {
+    if (element.name.length > 0) {
+      data[formIdentifier][element.name] = element.value;
+    }
+  }
+  return data;
+};
 
-  var map;
-  var dir;
 
-  map = L.map('map', {
-    layers: MQ.mapLayer(),
-    center: center,
-    zoom: zoom
+submitButton.onclick = event => {
+  data = getFormData();
+  localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+  const message = "Form draft has been saved!";
+  displayAlert(message);
+};
+
+const displayAlert = message => {
+  alertBox.innerText = message;
+  alertBox.style.display = "block";
+  setTimeout(function () {
+    alertBox.style.display = "none";
+  }, 1000);
+};
+var lang;
+var lat;
+var map;
+var currentLocation = new Array();
+var destLocation = new Array();
+const populateForm = () => {
+  var i = 0;
+  if (localStorage.key(formIdentifier)) {
+
+    const savedData = JSON.parse(localStorage.getItem(formIdentifier)); // get and parse the saved data from localStorage
+    console.log(savedData);
+    if(savedData != null){
+      for (const element of formElements) {
+        if (element.name in savedData) {
+          element.value = savedData[element.name];
+          if (i < 1) {
+            currentLocation.push(savedData[element.name]);
+            i++;
+          } else {
+            destLocation.push(savedData[element.name])
+          }
+        }
+      }
+    }
+    const message = "Form has been refilled with saved data!";
+    currentLocation = String(currentLocation[0]).split(' ');
+    destLocation = String(destLocation[0]).split(' ');
+    console.log(currentLocation);
+    console.log(destLocation);
+    displayAlert(message);
+  }
+};
+
+async function positionAjax(params) {
+  var position;
+  await $.ajax({
+    url: "http://api.ipstack.com/176.204.96.32?access_key=480b880c9a30256f5f70aa7c8d5501bf",
+    type: "GET",
+    success: function (data) {
+      lang = data.longitude;
+      lat = data.latitude;
+      console.log(lat);
+      console.log(lang);
+      position = [lat, lang];
+    },
+    error: function (data) {
+      console.error(data);
+      position = '';
+    }
   });
-
-  dir = MQ.routing.directions();
-
-  dir.route({
-    locations: locations
-  });
-
-  map.addLayer(MQ.routing.routeLayer({
-    directions: dir,
-    fitBounds: true
-  }));
+  return position;
 }
+
+async function initializeMap() {
+  if (currentLocation[0] === "" || currentLocation.length === 0 || currentLocation[0] === "undefined" || destLocation[0] === "undefined" || destLocation[0] === "" || destLocation.length === 0) {
+    var position = await positionAjax();
+    map = L.map('map').setView(position, 13);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+    $('#currentLocation').val(position[0] + " " + position[1]);
+    L.marker([lat, lang]).addTo(map)
+      .bindPopup('Please enter your current <br> and destination locations')
+      .openPopup();
+      console.log('sass');
+  } else {
+    console.log('21212');
+    map = L.map('map').setView(currentLocation, 13);
+    var routeControl = L.Routing.control({
+      waypoints: [
+        L.latLng(currentLocation[0], currentLocation[1]),
+        L.latLng(destLocation[0], destLocation[1])
+      ],
+    }).addTo(map);
+
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+    var routeArray = new Array();
+    routeArray = routeControl.getWaypoints();
+    var json = JSON.stringify(routeArray);
+  }
+
+
+}
+
+
+var directions;
+var chkReadyState = setInterval(function() {
+  if (document.readyState == "complete") {
+      // clear the interval
+      directions = $('.leaflet-routing-alt').children().children().children().children();
+
+      clearInterval(chkReadyState);
+      for (let index = 0; index < directions.length; index++) {
+        directions[index] = directions[index];
+        console.log(directions[index]);
+      }
+  }
+}, 1000);
+
+document.onload = populateForm(); // populate the form when the document is loaded
+document.onload = initializeMap();
+
+// function submitForm(event) {
+//   let dest = String($('#destLocation').val()).split(' ');
+//   let current = String($('#currentLocation').val()).split(' ');
+//   console.log(dest);
+//   console.log(current);
+//   map.invalidateSize(); // should work then
+
+//   var routeControl = L.Routing.control({
+//     waypoints: [
+//       L.latLng(current[0], current[1]),
+//       L.latLng(dest[0], dest[1])
+//     ],
+//   }).addTo(map);
+//   console.log('dsd');
+
+//   var routeArray = new Array();
+//   routeArray = routeControl.getWaypoints();
+//   var json = JSON.stringify(routeArray);
+//   console.log(json);
+
+// }
--- resources/js/server.js
+++ resources/js/server.js
@@ -1,26 +1,39 @@
+// simple HTTP server using TCP sockets
 var net = require('net');
 var fs = require('fs');
-var server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
-	socket.pipe(socket);
+var server = net.createServer(function (socket) {
+
+  socket.on('data', function (data) {
+    console.log('Received: ' + data);
+    r = data.toString();
+    console.log(r.length); // figure out the length for the first GET
+    if (r.substring(0, 3) == "GET" & r.length <= 600) { // if GET only
+      // Server is not checking to make sure that the client 
+      // actually sent a well-formed header.
+
+      //socket.write("OK");
+      socket.write("HTTP/1.1 200 OK\n");
+
+      fs.readFile('index.html', 'utf8', function (err, contents) {
+        console.log(contents);
+				socket.write("Content-Length:"+contents.length);
+				socket.write("\n\n"); // two carriage returns
+				socket.write(contents);
+      });
+
+    } else console.log(r); // show the actual message
+  });
+  socket.on('close', function () {
+    console.log('Connection closed');
+  });
+  socket.on('end', function () {
+    console.log('client disconnected');
+  });
+
+  socket.on('error', function () {
+    console.log('client disconnected');
+  });
 });
-
-server.listen(1337, '127.0.0.1');
-
-var net = require('net');
-
-var client = new net.Socket();
-client.connect(1337, '127.0.0.1', function() {
-	console.log('Connected');
-  client.write('Hello, server!');
-  client.end();
-});
-
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
-
-client.on('close', function() {
-	console.log('Connection closed');
+server.listen(5500, function () {
+  console.log('server is listening on port 5500');
 });
